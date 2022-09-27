d8a00684325beade755f04a4842bfad7ede34251
PrashantNandanvar <nandanwarp2905@gmail.com>
2022-09-27 19:14:37 +0530

    created admin dashboard

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -26,6 +26,9 @@ import { getError } from "./utils";
 import axios from "axios";
 import SearchBox from "./components/SearchBox";
 import SearchScreen from "./screens/SearchScreen";
+import ProductedRoute from "./components/ProductedRoute";
+import DashboardScreen from "./screens/DashboardScreen";
+import AdminRoute from "./components/AdminRoute";
 
 function App() {
   const { state, dispatch: ctxDispatch } = useContext(Store);
@@ -110,6 +113,19 @@ function App() {
                       Sign In
                     </Link>
                   )}
+                  {userInfo && userInfo.isAdmin && (
+                    <NavDropdown title="Admin" id="admin-nav-dropdown">
+                      <LinkContainer to="/admin/dashboard">
+                        <NavDropdown.Item>Dashboard</NavDropdown.Item>
+                      </LinkContainer>
+                      <LinkContainer to="/admin/orderlist">
+                        <NavDropdown.Item>Orders</NavDropdown.Item>
+                      </LinkContainer>
+                      <LinkContainer to="/admin/userlist">
+                        <NavDropdown.Item>Users</NavDropdown.Item>
+                      </LinkContainer>
+                    </NavDropdown>
+                  )}
                 </Nav>
               </Navbar.Collapse>
             </Container>
@@ -146,12 +162,35 @@ function App() {
               <Route path="/search" element={<SearchScreen />} />
               <Route path="/signin" element={<SigninScreen />} />
               <Route path="/signup" element={<SignupScreen />} />
-              <Route path="/profile" element={<ProfileScreen />} />
+              <Route
+                path="/profile"
+                element={
+                  <ProductedRoute>
+                    <ProfileScreen />
+                  </ProductedRoute>
+                }
+              />
               <Route path="/shipping" element={<ShippingAddressScreen />} />
               <Route path="/payment" element={<PaymentMethodScreen />} />
               <Route path="/placeorder" element={<PlaceOrderScreen />} />
-              <Route path="/order/:id" element={<OrderScreen />} />
-              <Route path="/orderhistory" element={<OrderHistoryScreen />} />
+              <Route
+                path="/order/:id"
+                element={
+                  <ProductedRoute>
+                    <OrderScreen />
+                  </ProductedRoute>
+                }
+              />
+              <Route
+                path="/orderhistory"
+                element={
+                  <ProductedRoute>
+                    <OrderHistoryScreen />
+                  </ProductedRoute>
+                }
+              />
+              {/* Admin Routes */}
+              <Route path="/admin/dashboard" element={<AdminRoute><DashboardScreen/></AdminRoute>}/>
               <Route path="/" element={<HomeScreen />} />
             </Routes>
           </Container>
